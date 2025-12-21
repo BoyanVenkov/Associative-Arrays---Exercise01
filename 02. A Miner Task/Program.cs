bf88aac6ff8d4fd66aab8fea0e585114ad515dc6
Boyan Venkov <boyanvenkov@yahoo.com>
2025-12-21 11:47:15 +0200

    Added a New Project

---
+++ 02. A Miner Task/Program.cs
@@ -0,0 +1,23 @@
+﻿Dictionary<string, int> output = new Dictionary<string, int>();
+
+while (true)
+{
+    string resource = Console.ReadLine();
+    if (resource == "stop")
+    {
+        break;
+    }
+    int quantity = int.Parse(Console.ReadLine());
+    if (!output.ContainsKey(resource))
+    {
+        output.Add(resource, quantity);
+    }
+    else
+    {
+        output[resource] += quantity;
+    }
+}
+foreach (var keyValuePair in output)
+{
+    Console.WriteLine($"{keyValuePair.Key} -> {keyValuePair.Value}");
+}
