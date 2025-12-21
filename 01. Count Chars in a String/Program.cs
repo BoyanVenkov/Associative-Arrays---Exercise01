adb9b1ae70f47148d7141404f61be5c2e11895c1
Boyan Venkov <boyanvenkov@yahoo.com>
2025-12-21 11:25:33 +0200

    Add project files.

---
+++ 01. Count Chars in a String/Program.cs
@@ -0,0 +1,25 @@
+﻿string input = Console.ReadLine();
+
+Dictionary<char, int> output = new Dictionary<char, int>();
+
+char[] inputChars = input.ToCharArray();
+
+for (int i = 0; i< inputChars.Length; i++)
+{
+    if (inputChars[i] != ' ')
+    {
+        if (!output.ContainsKey(inputChars[i]))
+        {
+            output[inputChars[i]] = 1;
+        }
+        else 
+        {
+            output[inputChars[i]]++;
+        }
+    }
+}
+foreach (var keyValuePair in output)
+{
+    Console.WriteLine($"{keyValuePair.Key} -> {keyValuePair.Value}");
+
+}
