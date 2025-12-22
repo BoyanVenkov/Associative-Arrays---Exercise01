5201606e605eb2cbc8e5099fb624f04261f964df
Boyan Venkov <boyanvenkov@yahoo.com>
2025-12-22 22:51:18 +0200

    Added a New Project

---
+++ 04. SoftUni Parking/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Dictionary<string, string> users = new Dictionary<string, string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string action = command[0];
+            string username = command[1];
+
+            if (action == "register")
+            {
+                string licensePlate = command[2];
+
+                if (users.ContainsKey(username))
+                {
+                    Console.WriteLine($"ERROR: already registered with plate number {users[username]}");
+                }
+                else
+                {
+                    users.Add(username, licensePlate);
+                    Console.WriteLine($"{username} registered {licensePlate} successfully");
+                }
+            }
+            else if (action == "unregister")
+            {
+                if (!users.ContainsKey(username))
+                {
+                    Console.WriteLine($"ERROR: user {username} not found");
+                }
+                else
+                {
+                    users.Remove(username);
+                    Console.WriteLine($"{username} unregistered successfully");
+                }
+            }
+        }
+        foreach (var user in users)
+        {
+            Console.WriteLine($"{user.Key} => {user.Value}");
+        }
+    }
+}
